b9ed27a092d0206fadbbdab2350313c8e6eeda3c
znowflak3 <joakim.znoflinga@yahoo.com>
2020-01-07 11:48:51 +0100

    quiz component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz.js'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
